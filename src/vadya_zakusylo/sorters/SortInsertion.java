2e5d2b3a1fc76c0d42c1627e126a1de9913c5ff6
vadyaChmil <vadya.chmil@gmail.com>
2015-08-25 18:32:24 +0300

    ...

--- src/vadya_zakusylo/sorters/SortInsertion.java
+++ src/vadya_zakusylo/sorters/SortInsertion.java
@@ -7,11 +7,11 @@ public class SortInsertion {
 	public static void main(String[] args) {
 		int[] a = { 5, 3, 5, 6, 8, 9, 10, 2, 1, 6, 3 };
 		for (int index = 1; index < a.length; index++) {
-			int workedIndex = a[index];
+			int workedValue = a[index];
 			int prevIndex = index - 1;
-			while (prevIndex >= 0 && a[prevIndex] > workedIndex) {
+			while (prevIndex >= 0 && a[prevIndex] > workedValue) {
 				a[prevIndex + 1] = a[prevIndex];
-				a[prevIndex] = workedIndex;
+				a[prevIndex] = workedValue;
 				prevIndex--;
 			}
 		}
